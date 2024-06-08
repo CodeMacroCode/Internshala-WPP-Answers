d4c0dbb37e8b8a5c26f389e2f75f1f00cb231aa1
Macro-internshala <macro-internshala@test.com>
2024-06-08 12:33:41 +0530

    added js code

--- WPP_day1ans/day1ans.js
+++ WPP_day1ans/day1ans.js
@@ -3,4 +3,18 @@ Sample Output:
 Today is: Tuesday. 
 Current time is: 10 PM : 30 : 38 */
 
-let day;
+let today = new Date;
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let day = dayList[today.getDay()];
+let hours = today.getHours();
+let meridiem = "AM";
+let min = today.getMinutes();
+let sec = today.getSeconds();
+
+if (hours > 12) {
+    hours -= 12;
+}else {
+    meridiem = "PM";
+}
+
+console.log(`Today is: ${day}. \nCurrent time is: ${hours} ${meridiem} : ${min} : ${sec}`)
